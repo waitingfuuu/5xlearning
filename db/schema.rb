eae27e4f01908b3571de1ec6642c580ad7b2cd62
weiting <weitingmm@gmail.com>
2022-07-05 18:38:36 +0800

    delete files about users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_085441) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_04_082639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,15 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_085441) do
     t.integer "end_time"
     t.integer "priority"
     t.integer "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.integer "_id"
-    t.string "username"
-    t.string "pw"
-    t.string "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
