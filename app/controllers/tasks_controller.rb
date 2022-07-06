b8fe33d2755fb32bb4724e17d64ab1c8f96acd62
weiting <weitingmm@gmail.com>
2022-07-06 18:59:12 +0800

    Update flash

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :find_task, only: [:update, :edit]
+
   def index
     @tasks = Task.all
   end
@@ -10,36 +11,45 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
+
     if @task.save
+      flash[:notice] = "Task: #{@task.title} successfully created"
+
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @task.update(task_params)
-      redirect_to root_path
+      flash[:notice] = "Task: #{@task.title} successfully edited"
+
+      redirect_to  root_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
   def destroy
     Task.destroy(params[:id])
+
+    flash[:notice] = "Task successfully deleted"
     redirect_to root_path
   end
 
+  def show; end
+
   private
-   def task_params
-     params.require(:task).permit(:user_id, :title, :content, :tag, :build_time, :start_time, :end_time, :priority, :state)
-   end
 
-   def find_task
-    @task = Task.find(params[:id])
-  end
+    def task_params
+      params.require(:task).permit(:user_id, :title, :content, :tag, 
+                                  :build_time, :start_time, :end_time, :priority, :state)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])
+    end
 end
-
