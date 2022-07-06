fe9404d06bb8d437400ddcb04a4445a2b237189b
weiting <weitingmm@gmail.com>
2022-07-06 17:40:48 +0800

    Update root_path and before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:update, :edit]
   def index
     @tasks = Task.all
   end
@@ -10,36 +11,35 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to '/'
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to '/'
+      redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-
-  def edit
-    @task = Task.find_by(id: params[:id])
-  end
-
+  
   def destroy
     Task.destroy(params[:id])
-    redirect_to('/')
+    redirect_to root_path
   end
 
   private
    def task_params
      params.require(:task).permit(:user_id, :title, :content, :tag, :build_time, :start_time, :end_time, :priority, :state)
    end
+
+   def find_task
+    @task = Task.find(params[:id])
+  end
 end
+
