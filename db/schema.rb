3bb952dabfbd6510dbea3bbe2349c920cd99f24a
weiting <weitingmm@gmail.com>
2022-07-07 15:13:05 +0800

    delete build_time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_082639) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_070832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_082639) do
     t.string "title"
     t.text "content"
     t.string "tag"
-    t.date "build_time"
     t.date "start_time"
     t.date "end_time"
     t.string "priority"
