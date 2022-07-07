833af4f492d85f1a9952572062faa28fc86a03b1
weiting <weitingmm@gmail.com>
2022-07-07 15:29:18 +0800

    change datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_070832) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_072348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_070832) do
     t.string "title"
     t.text "content"
     t.string "tag"
-    t.date "start_time"
-    t.date "end_time"
+    t.datetime "start_time", precision: nil
+    t.datetime "end_time", precision: nil
     t.string "priority"
     t.string "state"
     t.datetime "created_at", null: false
