7a374f2329270bfd1cdf92e9f85218f8f1240213
weiting <weitingmm@gmail.com>
2022-07-13 10:49:44 +0800

    Update task_spec

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -10,8 +10,8 @@ FactoryBot.define do
     tag { Faker::String.random(length: 4) }
     start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
     end_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1, format: :default) }
-    priority { '高' }
-    state { '處理中' }
+    priority { '1' }
+    state { '0' }
   end
 end
 
@@ -23,8 +23,8 @@ describe 'tasks', type: :feature do
 
   context 'when create task' do
     it 'link to new task path' do
-      click_link I18n.t(:create_task)
-      expect(page).to have_content(I18n.t(:create_task))
+      click_link I18n.t('form.create_task')
+      expect(page).to have_content(I18n.t('form.create_task'))
     end
 
     it 'shows new task' do
@@ -32,7 +32,7 @@ describe 'tasks', type: :feature do
       within('#task_form') do
         fill_in 'task[title]', with: 'newtask'
       end
-      click_button I18n.t(:submit)
+      click_button I18n.t('form.submitBtn')
       expect(page).to have_content('newtask')
     end
   end
@@ -43,11 +43,11 @@ describe 'tasks', type: :feature do
     end
 
     it 'shows edited task' do
-      click_link I18n.t(:edit)
+      click_link I18n.t('index.edit')
       within('#task_form') do
         fill_in 'task[title]', with: 'change-title'
       end
-      click_button I18n.t(:submit)
+      click_button I18n.t('form.submitBtn')
       expect(page).to have_content('change-title')
     end
   end
@@ -58,7 +58,7 @@ describe 'tasks', type: :feature do
     end
 
     it 'deleted the task' do
-      click_link I18n.t(:delete)
+      click_link I18n.t('index.delete')
       expect(Task.count).to eq(0)
     end
   end
