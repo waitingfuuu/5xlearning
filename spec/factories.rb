0e735aafbdda3e566903d5235026b80c2e35e815
weiting <weitingmm@gmail.com>
2022-07-13 11:22:50 +0800

    Update FactoryBot folder

---
+++ spec/factories.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+FactoryBot.define do
+  factory :task do
+    user_id { Faker::Number.decimal_part(digits: 2) }
+    title { Faker::Job.title }
+    content { Faker::String.random }
+    tag { Faker::String.random(length: 4) }
+    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
+    end_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1, format: :default) }
+    priority { '1' }
+    state { '0' }
+  end
+end
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -2,19 +2,6 @@
 
 require 'rails_helper'
 
-FactoryBot.define do
-  factory :task do
-    user_id { Faker::Number.decimal_part(digits: 2) }
-    title { Faker::Job.title }
-    content { Faker::String.random }
-    tag { Faker::String.random(length: 4) }
-    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
-    end_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1, format: :default) }
-    priority { '1' }
-    state { '0' }
-  end
-end
-
 describe 'tasks', type: :feature do
   before do
     create(:task, title: 'test-task \u0000')
