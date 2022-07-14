704e8ede787533c65984439c6253a69c5b093c4c
weiting <weitingmm@gmail.com>
2022-07-14 17:23:50 +0800

    Update Faker Lorem

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,9 +3,9 @@
 FactoryBot.define do
   factory :task do
     user_id { Faker::Number.decimal_part(digits: 2) }
-    title { Faker::Job.title }
-    content { Faker::String.random.tr("\u0000", '') }
-    tag { Faker::String.random(length: 4) }
+    title { Faker::Lorem.words }
+    content { Faker::Lorem.paragraphs }
+    tag { Faker::Lorem.words }
     start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default) }
     end_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1, format: :default) }
     priority { '1' }
