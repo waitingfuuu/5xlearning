504de4a53a2945abb45735c7a68703250c10b66e
weiting <weitingmm@gmail.com>
2022-07-19 12:25:39 +0800

    Add search model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Task, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:title) }
   end
+
+  describe 'when search tasks' do
+    it 'finds a task by title' do
+      task = create(:task, title: 'test')
+      result = described_class.where(title: 'test')
+      expect(result).to eq([task])
+    end
+  end
 end
