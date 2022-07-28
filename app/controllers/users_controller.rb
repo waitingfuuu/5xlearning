94fd29d4946745557e4a26eeefc5fb9109a257e2
weiting <weitingmm@gmail.com>
2022-07-28 11:49:45 +0800

    Fix password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    @user.password_digest = BCrypt::Password.create(user_params[:password])
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: t('flash.signup_successfully')
